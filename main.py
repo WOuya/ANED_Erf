8c79814fa4d6881a9fe1a4c43a82890048510bbc
Boris Ouya <boris.ouya.pro@gmail.com>
2024-07-25 18:46:13 +0200

    Meilleur algorithme de calcul

--- main.py
+++ main.py
@@ -12,10 +12,30 @@ def naive_erf(x,N):
 
     return approximation,terms
 
+
+def better_erf(x,k):
+    ak = 2 / math.sqrt(math.pi) * x * math.exp(-x**2) 
+    approximation = 0
+    N = 0
+    while True:
+        approximation += ak
+        if N > x**2 and ak * x**2 /(N - x**2) <= 10**(-k):
+            break
+        N += 1
+        ak = ak * 4*N / ((2*N +1)*(2*N)) * x**2
+
+    return approximation
+
+        
+
+
+
+
 if __name__ == "__main__":
     N = 150
-    x = 3
-    N_visual = x**2
+    x = 10
+    N_visual = x**2 +1 
+    k = 15
 
     approximation,terms = naive_erf(x,N)
 
@@ -23,3 +43,13 @@ if __name__ == "__main__":
     print(f"Naive Approximation of Erf({x}) for  N = {N}: {approximation}")
     print(f"List of terms up until term  #{N_visual}: \\")
     print(terms[:N_visual])
+
+
+    approximation = better_erf(x,k)
+
+    print("===Approximation w/o compensation===")
+    print(f"Approximation of Erf({x}) with precision 10^(-{k}) : {approximation}")
+
+
+    print(f"Valeur python: {math.erf(x)}")
+    print(f"Différence: {abs(math.erf(3) - approximation)}")
